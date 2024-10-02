06e8e89f454e7e74fd6cc79d01383dca5ff3eb41
Nông Nguyễn Thành <thanhnn16.work@gmail.com>
2024-10-02 20:13:35 +0700

    refactor: Consolidate redundant code in AppHeader and MenuPC components
    Consolidate the redundant code for the "Khuyến mãi" and "Hỗ Trợ" items in the AppHeader and MenuPC components. This refactor removes the duplicate code and consolidates it into a single kf-box div in the AppHeader component. The changes improve code organization and reduce duplication.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -17,8 +17,8 @@
                             src="https://cms.t79.bet/storage/uploads/config/202409/25/config_1727248104_7vGCKJpUyb.png"
                             alt="logoName"></a>
                 </div>
-                <MenuPC />
-                
+                <MenuDesktop />
+
             </div>
         </div>
         <div class="header03 sp">
